b9b6cc7708a0dbb7a39773efd1c143542f6678bc
Aruna Turay <tura_arun@yahoo.com>
2023-08-18 16:55:41 -0400

    all good

--- migrations/env.py
+++ migrations/env.py
@@ -13,6 +13,7 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+    
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -64,7 +65,7 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata, render_as_batch=True
         )
 
         with context.begin_transaction():
--- models.py
+++ models.py
@@ -4,13 +4,15 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
     
     id = Column(Integer, primary_key=True)
-    username = Column(String)
+    username = Column(String, unique=True)
+    email = Column(String(55))
     
     def __repr__(self):
         return f"\n<User" \
             + f"id={self.id}, " \
             + f"username={self.username}, " \
+            + f"email={self.emal}," \
             + ">"
